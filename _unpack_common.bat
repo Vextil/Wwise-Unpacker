288dcfe4a92aad9bd54b268826cef2b45aec147e
mortalis13 <stangerroman@gmail.com>
2020-08-12 22:08:48 +0200

    -script for unpacking raw WAV audio

--- _unpack_common.bat
+++ _unpack_common.bat
@@ -2,15 +2,24 @@
 mkdir "Tools\Decoding"
 mkdir "OGG"
 mkdir "MP3"
+mkdir "WAV"
 
 set TYPE=%1
 echo -- Unpacking to '%TYPE%'
 @echo on
 
+if "%TYPE%"=="WAV" (
+FOR %%a IN ("Game Files\*.PCK") DO ("Tools\quickbms.exe" "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
+FOR %%b IN ("Game Files\*.BNK") DO ("Tools\bnkextr.exe" "%%b" & MOVE *.wav "Tools\Decoding")
+FOR %%a IN ("Tools\Decoding\*.WEM") DO ("Tools\vgmstream-cli.exe" -o "WAV\%%~na.wav" "%%a")
+)
+
+if "%TYPE%"=="OGG" (
 FOR %%a IN ("Game Files\*.PCK") DO ("Tools\quickbms.exe" "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
 FOR %%b IN ("Game Files\*.BNK") DO ("Tools\bnkextr.exe" "%%b" & MOVE *.wav "Tools\Decoding")
 FOR %%c IN (Tools\Decoding\*.WAV) DO ("Tools\ww2ogg.exe" "%%c" --pcb Tools\packed_codebooks_aoTuV_603.bin & DEL "%%c")
 FOR %%d IN (Tools\Decoding\*.OGG) DO ("Tools\revorb.exe" "%%d" & MOVE "%%d" "%TYPE%")
+)
 
 if "%TYPE%"=="MP3" (
 FOR %%e IN (MP3\*.OGG) DO ("Tools\ffmpeg.exe" -i "%%e" -acodec libmp3lame -q:a 0 -y "MP3\%%~ne.mp3" & DEL "%%e")
